a337d4db2848527ada67fe38fc74b5008ad5a108
fRed <fredbrossard@live.fr>
2026-01-22 15:46:17 +0100

    feat: entities architecture with interface, obj and model for player and enemy.

---
+++ Assets/Game/Entity/Enemy/Generic/Enemy.cs
@@ -0,0 +1,28 @@
+using Game.Entity.Generic;
+using Game.Entity.Model;
+using UnityEngine;
+
+namespace Game.Entity.Enemy
+{
+    public class Enemy : MonoBehaviour, IEntity<EnemyModel>
+    {
+        public bool IsAlive { get; set; }
+
+        [field:SerializeField] public EnemyModel Model { get; private set; }
+
+        public void Disable()
+        {
+            
+        }
+
+        public void Enable()
+        {
+            
+        }
+
+        public void Initialize()
+        {
+           
+        }
+    }
+}
---
+++ Assets/Game/Entity/Enemy/Generic/EnemyModel.cs
@@ -0,0 +1,17 @@
+﻿using Game.Entity.Generic;
+using System;
+using UnityEngine;
+
+namespace Game.Entity.Model
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "Data/Model/EnemyModel", order = 2), Serializable]
+    public class EnemyModel : ScriptableObject, IEntityModel
+	{
+        public Sprite sprite;
+
+        public uint initialMaxHealth;
+        public byte initialLvl;
+        public uint initialAttack;
+        public float cooldownAttack;
+    }
+}
